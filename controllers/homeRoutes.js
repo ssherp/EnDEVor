3d37ea2f22527ac1b7f97963fb670d40264a63c4
aaron <aaronlamfinance@gmail.com>
2023-08-11 12:10:11 -0700

    Add empty .get function for home route

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const { } = require('../models')
+
+router.get('/', async (req, rse) => {
+	try {
+
+	} catch {
+		res.status(500).json(err);
+	}
+
+
+})
+
+module.exports = router;
