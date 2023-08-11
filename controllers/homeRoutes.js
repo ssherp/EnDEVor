ac270e223b3016b283d7835020a952f8e3c095ae
ssherp <ssherpa89@gmail.com>
2023-08-11 13:29:15 -0700

    add signin js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { Service, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/homepage');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
