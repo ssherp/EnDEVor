3e474ad190cd10bd99d35c871a3f706bd4979652
Lainey Creighton <dev.lainey@gmail.com>
2023-08-12 17:48:45 -0400

    add a create quote route

---
+++ controllers/api/quoteRoutes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const { Trip } = require('../../models');
+
+// CREATE a quote
+router.post('/', async (req, res) => {
+  try {
+    const quoteData = await Quote.create(req.body);
+    res.status(200).json(quoteData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// GET all quotes
+router.delete('/:id', async (req, res) => {
+  try {
+    const tripData = await Trip.destroy({
+      where: { id: req.params.id }
+    });
+    if (!tripData) {
+      res.status(404).json({ message: 'No trip with this id!' });
+      return;
+    }
+    res.status(200).json(tripData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a quotes
+router.delete('/:id', async (req, res) => {
+    try {
+      const tripData = await Trip.destroy({
+        where: { id: req.params.id }
+      });
+      if (!tripData) {
+        res.status(404).json({ message: 'No trip with this id!' });
+        return;
+      }
+      res.status(200).json(tripData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
