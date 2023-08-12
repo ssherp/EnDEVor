8bdb06d7e2d8e81a677b76a584c3b40d71c18c93
Lainey Creighton <dev.lainey@gmail.com>
2023-08-12 19:01:21 -0400

    define get a single quote route

--- controllers/api/quoteRoutes.js
+++ controllers/api/quoteRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Trip } = require('../../models');
+const { Quote } = require('../../models');
 
 // CREATE a quote
 router.post('/', async (req, res) => {
@@ -12,22 +12,37 @@ router.post('/', async (req, res) => {
 });
 
 // GET all quotes
-router.delete('/:id', async (req, res) => {
-  try {
-    const tripData = await Trip.destroy({
-      where: { id: req.params.id }
-    });
-    if (!tripData) {
-      res.status(404).json({ message: 'No trip with this id!' });
-      return;
+router.get('/', async (req, res) => {
+    try {
+      const quoteData = await Quote.findAll();
+      console.log(quoteDataData);
+      const quotes = quoteData.map((quote) => quote.get({ plain: true }));
+      console.log("quotes", quotes);
+      res.status(200).json(quoteData);
+    } catch (err) {
+      res.status(500).json(err);
     }
-    res.status(200).json(tripData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+  });
 
-// GET a quotes
+// GET a quote
+router.get('/:id', async (req, res) => {
+    try {
+      const quoteData = await quote.findByPk(req.params.id, {
+        include: [{ model: Quote, through: Trip, as: 'location_travellers' }]
+      });
+  
+      if (!quoteData) {
+        res.status(404).json({ message: 'No location found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(locationData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+// DELETE a quote
 router.delete('/:id', async (req, res) => {
     try {
       const tripData = await Trip.destroy({
