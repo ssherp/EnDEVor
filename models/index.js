5aa89871472fa7286ec81f1329993b893a6a5bbc
aaron <aaronlamfinance@gmail.com>
2023-08-13 00:33:01 -0700

    attempt 2

--- models/index.js
+++ models/index.js
@@ -1,44 +1,44 @@
-const User = require('./User');
-const Service = require('./Service');
-const Quote_Item = require('./Quote_Item');
-const Quote = require('./Quote');
+const User = require("./User");
+const Service = require("./Service");
+const Quote_Item = require("./Quote_Item");
+const Quote = require("./Quote");
 
 User.hasMany(Service, {
-    foreignKey: 'user_id'
+	foreignKey: "user_id",
 });
 
 User.hasMany(Quote, {
-    foreignKey: 'user_id'
+	foreignKey: "user_id",
 });
 
 User.hasMany(Quote_Item, {
-    foreignKey: 'user_id'
+	foreignKey: "user_id",
 });
 
 Service.belongsTo(User, {
-    foreignKey: "user_id",
-    onDelete: 'CASCADE'
+	foreignKey: "user_id",
+	onDelete: "CASCADE"
 });
 
 Service.hasMany(Quote_Item, {
-    foreignKey: "service_id"
+	foreignKey: "service_id"
 });
 
 Quote_Item.belongsTo(Service, {
-    foreignKey: "service_id"
+	foreignKey: "service_id"
 });
 
 Quote_Item.belongsTo(Quote, {
-    foreignKey: "quote_id"
+	foreignKey: "quote_id",
 });
 
 Quote.hasMany(Quote_Item, {
-    foreignKey: 'quote_item_id',
-    onDelete: 'CASCADE'
+	foreignKey: "quote_item_id",
+	onDelete: "CASCADE"
 });
 
 Quote.belongsTo(User, {
-    foreignKey: 'user_id'
+	foreignKey: "user_id",
 });
 
 module.exports = { User, Service, Quote, Quote_Item };
