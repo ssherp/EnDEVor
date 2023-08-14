fdc0f42d65094085711503e43ded29f0d090a6b2
aaron <aaronlamfinance@gmail.com>
2023-08-13 17:30:48 -0700

    Add create route

--- controllers/api/serviceRoutes.js
+++ controllers/api/serviceRoutes.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
 const { Service } = require('../../models');
 
+//create new service to services list 
+router.post('/', async (req,res) => {
+	try {
+		const serviceData = await Service.create({
+			service_name: req.body.name,
+			service_description: req.body.description,
+			service_price: req.body.price
+		});
+		res.status(200).json(serviceData)
+	} catch {
+		req.status(400).json(err);
+	} 
+});
+
 //get all services we offer for services page 
 router.get ('/', async (req, res) => {
 	try {
@@ -13,6 +27,24 @@ router.get ('/', async (req, res) => {
 		}
 	catch { 
 		res.status(400).json(err);
+	}
+});
+
+//get single service
+router.get('./services/:id', async (req, res) => {
+	//if not logged in, redirect
+	if (!req.session.loggedIn) { 
+		res.redirect('/login') //temporary pathway for login page
+	}
+	try {
+		const servicesData = await Service.findByPk(req.params.id) {
+			res.status(200).json(quoteData);
+			if (!quoteData) {
+				res.status(404).json({ message: 'Service not found' });
+				return;
+			}
+
+		
 	}
 });
 
