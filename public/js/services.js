d3477837e9430fa3cf73f6476fbfecb0707520b6
aaron <aaronlamfinance@gmail.com>
2023-08-13 21:57:28 -0700

    Start public javascript

--- public/js/services.js
+++ public/js/services.js
@@ -1,7 +1,12 @@
-const addServiceHandler = async(event) => {
+async function servicesListHandler(event) {
 	event.preventDefault();
+	const serviceAddBtn = document.querySelector('#serviceAddBtn');
+	const serviceName = document.querySelector('#service-name').value;
+	const servicePrice = document.querySelector('#service-price').value;
+	const serviceDescription = document.querySelector('#service-description').value
+
+
 }
 
+document.querySelector('services-list').addEventListener('submit', servicesListHandler);
 
-
-document.getElementById("addButton")
