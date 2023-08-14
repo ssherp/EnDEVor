fffa546dc1915b9de6515721866c5ac86999c8d9
aaron <aaronlamfinance@gmail.com>
2023-08-13 22:32:01 -0700

    add html fixes to services_details handlebar

--- public/js/services.js
+++ public/js/services.js
@@ -1,6 +1,6 @@
+//using submit event listener, all changes will be at click of button in function.
 async function servicesListHandler(event) {
 	event.preventDefault();
-	const serviceAddBtn = document.querySelector('#serviceAddBtn');
 	const service_name = document.querySelector('#service-name').value;
 	const description = document.querySelector('#service-price').value;
 	const service_price = document.querySelector('#service-description').value
@@ -21,6 +21,14 @@ async function servicesListHandler(event) {
 	}
 }
 
+async function editServiceHandler(event) {
+	event.preventDefault();
+	const edit_name = document.querySelector('#edit-name').value;
+	const edit_description = document.querySelector('#edit_description').value;
+	const edit_price = document.querySelector('#edit-price').value;
+}
+
 document.querySelector('services-list').addEventListener('submit', servicesListHandler);
+document.querySelector('edit-service').addEventListener('submit', editServiceHandler);
 
 //refresher on fetch https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
