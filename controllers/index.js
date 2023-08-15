fb5e0c99283f0f4b43fbd525684962daf2d8d135
aaron <aaronlamfinance@gmail.com>
2023-08-15 00:42:47 -0700

    add add/edit service routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const serviceRoutes = require('./serviceRoutes');
+const servicesRoutes = require('./servicesRoutes');
+const editServiceRoutes = require('./editServicesRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/services', serviceRoutes);
+router.use('/services', servicesRoutes);
+router.use('/edit_service', );
 
 
 module.exports = router;
--- controllers/servicesRoutes.js
+++ controllers/servicesRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Service, User , Quote , Quote_Item , } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/service', withAuth, async (req, res)=>{
+router.get('/services', withAuth, async (req, res)=>{
 	try{
 		 res.render('add-service', { 
 			logged_in: req.session.logged_in 
