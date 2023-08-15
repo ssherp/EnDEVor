2240fb40eb405e547e633d3354dcf30027b1a43f
aaron <aaronlamfinance@gmail.com>
2023-08-15 00:53:42 -0700

    short comments

--- controllers/index.js
+++ controllers/index.js
@@ -12,9 +12,12 @@ const quoteFileRoutes = require('./quoteFileRoutes');
 //Not sure if different files are necessary
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+// services routes
 router.use('/services', servicesRoutes);
-router.use('/edit_service', editServiceRoutes);
+router.use('/services/edit', editServiceRoutes);
+// profile route -> when does user fill this out?
 router.use('/profile', profileRoutes);
+// quote routes
 router.use('/quote_form', quoteFormRoutes);
 router.use('/quote_file', quoteFileRoutes);
 
