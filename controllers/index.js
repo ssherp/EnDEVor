df7a71269b736226e9a48e466c616b657441568b
aaron <aaronlamfinance@gmail.com>
2023-08-15 00:51:19 -0700

    Complete user routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,12 +3,20 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const servicesRoutes = require('./servicesRoutes');
-const editServiceRoutes = require('./editServicesRoutes');
+const editServiceRoutes = require('./editServiceRoutes');
+const profileRoutes = require('./profileRoutes');
+const quoteFormRoutes = require('./quoteFormRoutes');
+const quoteFileRoutes = require('./quoteFileRoutes');
 
+
+//Not sure if different files are necessary
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/services', servicesRoutes);
-router.use('/edit_service', );
+router.use('/edit_service', editServiceRoutes);
+router.use('/profile', profileRoutes);
+router.use('/quote_form', quoteFormRoutes);
+router.use('/quote_file', quoteFileRoutes);
 
 
 module.exports = router;
