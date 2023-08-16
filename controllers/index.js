8b6b843fbfeb031664fea635f8c3150d8955a31b
ssherp <ssherpa89@gmail.com>
2023-08-15 18:32:39 -0700

    debugged profile page

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,6 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const servicesRoutes = require('./servicesRoutes');
 const editServiceRoutes = require('./editServiceRoutes');
-const profileRoutes = require('./profileRoutes');
 const quoteFormRoutes = require('./quoteFormRoutes');
 const quoteFileRoutes = require('./quoteFileRoutes');
 
@@ -16,7 +15,6 @@ router.use('/api', apiRoutes);
 router.use('/services', servicesRoutes);
 router.use('/services/edit', editServiceRoutes);
 // profile route -> when does user fill this out?
-router.use('/profile', profileRoutes);
 // quote routes
 router.use('/quote_form', quoteFormRoutes);
 router.use('/quote_file', quoteFileRoutes);
