80b872404cd1cfc3370b67fddff0b31a78b658c0
Lainey Creighton <dev.lainey@gmail.com>
2023-08-16 19:35:34 -0400

    add psuedocode for view services

---
+++ public/js/view-service.js
@@ -0,0 +1,16 @@
+//Get all services based on logged in user
+    //Write a function that is triggered on page load
+        //Make a get all services fetch
+            //select which parameters you want to extract
+            //serialize data
+        //Create a partial for each service item
+            //Make each service item a button
+            //Redirect to "EDIT SERVICE HANDLEBAR" for that specific service item
+
+//Add Event Listener for the "ADD ITEM" button
+    //Redirect to "ADD SERVICE HANDLEBAR"
+        //getElementById
+        //addEventListener
+        //write a function
+            //window.location.href="(file path)"
+                //set up the route for the "add service item" handlebar
