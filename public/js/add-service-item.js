4fd4e4d9ee8916bb94e701623cef13f3b7e6de71
Lainey Creighton <dev.lainey@gmail.com>
2023-08-16 20:12:54 -0400

    add psuedocode for the add services

---
+++ public/js/add-service-item.js
@@ -0,0 +1,6 @@
+//Create a new service item and save it to the service model
+    //create variables for the user inputs using querySelectors
+    //addEventListener for the save/add button
+        //call on the POST route to create new item
+        //use the accurate parameters
+            //name, description, and price
