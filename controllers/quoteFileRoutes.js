50a3d4024c02d08f17c322fa2046173c9e457416
Lainey Creighton <dev.lainey@gmail.com>
2023-08-19 17:32:19 -0400

    add styling to quote file

--- controllers/quoteFileRoutes.js
+++ controllers/quoteFileRoutes.js
@@ -23,7 +23,7 @@ router.get('/', withAuth, async (req, res)=>{
 			logged_in: req.session.logged_in 
 		 });
 	} catch(error) {
-		 res.status(500).json({ error, loggedIn: req.session.log_in }) //checks
+		 res.status(500).json({ error, loggedIn: req.session.log_in })
 	}
  })
 
