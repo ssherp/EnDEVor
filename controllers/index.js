92d6f0a86408d16dae0ab69f951ab2a8d1ec2f17
Lainey Creighton <dev.lainey@gmail.com>
2023-08-19 15:39:31 -0400

    organize routers

--- controllers/index.js
+++ controllers/index.js
@@ -1,25 +1,42 @@
+//-------------------- Requires --------------------//
+
+//Express Package
 const router = require('express').Router();
 
+
+//File Path Files
 const apiRoutes = require('./api/index');
 const homeRoutes = require('./homeRoutes');
 const serviceRoutes = require('./serviceRoutes');
 const quoteFormRoutes = require('./quoteFormRoutes');
 const quoteFileRoutes = require('./quoteFileRoutes');
  
+
+
+
+
+//-------------------- File Paths: Part I --------------------//
+
 //CRUD Routes
 router.use('/api', apiRoutes);
+
 
 //Render Routes
 router.use('/', homeRoutes);
 
+
 // Services Routes
 router.use('/services', serviceRoutes);
 
+
 //Create Quote Routes
 router.use('/quote_form', quoteFormRoutes);
+
 
 //View and Download Quote Routes
 router.use('/quote_file', quoteFileRoutes);
 
 
+
+//-------------------- Export Routes --------------------//
 module.exports = router;
